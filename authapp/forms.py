6c2251b8d817e19052530acbf2d081ea224f90b3
alex <schegoleff@gmail.com>
2021-04-04 13:16:00 +0300

    HW9

---
+++ authapp/forms.py
@@ -0,0 +1,62 @@
+import hashlib
+
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+
+
+from authapp.models import ShopUser
+
+
+class ShopUserLoginForm(AuthenticationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'password',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'email', 'first_name', 'age', 'avatar', 'password1', 'password2',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды!')
+        return data
+
+    def save(self, **kwargs):
+        user = super().save()
+        user.is_active = False
+        user.activation_key = hashlib.sha1(user.email.encode('utf8')).hexdigest()
+        user.save()
+        return user
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'email', 'first_name', 'age', 'avatar', 'password',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды!')
+        return data
