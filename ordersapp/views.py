0618cbcd8bd4a371422814beb93cf2350de47b2c
alexs6669 <schegoleff@gmail.com>
2021-04-16 11:24:27 +0300

    HW13.1

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,12 +2,13 @@ from django.db import transaction
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from basketapp.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderForm, OrderItemForm
 from ordersapp.models import Order, OrderItem
 
@@ -115,6 +116,14 @@ def order_forming_complete(request, pk):
 
     return HttpResponseRedirect(reverse('order:orders_list'))
 
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({'price': product.price})
+        else:
+            return JsonResponse({'price': 0})
 
 # @receiver(pre_save, sender=Basket)
 # @receiver(pre_save, sender=OrderItem)
