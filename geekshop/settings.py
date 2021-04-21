dea4dfbef45031cfd1a7480fcf1287939f9d503b
alexs6669 <schegoleff@gmail.com>
2021-04-21 10:51:15 +0300

    HW13.4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,8 +26,8 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
