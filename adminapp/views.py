484e65d225013d75ee2df7b5e3a4b7aa8d33b69a
alexs6669 <schegoleff@gmail.com>
2021-04-27 16:03:21 +0300

    исправляем неактивныек категории

--- adminapp/views.py
+++ adminapp/views.py
@@ -10,6 +10,27 @@ from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from mainapp.models import ProductCategory, Product
 
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.db import connection
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}:')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
+
 
 class UserListView(ListView):
     model = ShopUser
